077a3c2bd6fc2fb6c0546e435ba3020569f25067
Shreeka <Shreeka.Lohani@softwire.com>
2025-07-02 11:53:48 +0100

    part 2

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,18 +1,36 @@
 // This is our main function
 function fizzbuzz() :void {
-    for (let i = 1; i < 101; i++) {
+    for (let i = 195; i < 256; i++) {
+        let messages : string[] = new Array(5);
         let message : string = "";
-        if (i % 3 == 0 && i % 5 == 0) {
-            message = "FizzBuzz";
+
+        // for each rule, the corresponsing message is put in a unique index of messages
+        if (i % 3 == 0) {
+            messages[0] = "Fizz";
         }
-        else if (i % 3 == 0) {
-            message = "Fizz";
+        if (i % 5 == 0) {
+            messages[2] = "Buzz";
         }
-        else if (i % 5 == 0) {
-            message = "Buzz";
+        if (i % 7 == 0) {
+            messages[3] = "Bang";
+        }
+        if (i % 11 == 0) {
+            messages = new Array(6);
+            messages[4] = "Bong";
+        }
+        if (i % 13 == 0) {
+            messages[1] = "Fezz";
+        }
+        if (i % 17 == 0) {
+            messages = messages.reverse();
+        }
+
+        // generating final message
+        if (messages.toString() === (new Array(5)).toString()) {
+            message = i.toString();
         }
         else {
-            message = i.toString();
+            message = messages.join("");
         }
 
         console.log(message);
