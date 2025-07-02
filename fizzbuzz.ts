bb4c4f58e2811501a388ec81cb2389c03dfcb3d3
Shreeka <Shreeka.Lohani@softwire.com>
2025-07-02 09:56:02 +0100

    part 1 of fizzbuzz

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,8 +1,22 @@
 // This is our main function
 function fizzbuzz() :void {
-    console.log("Hello, World!");
+    for (let i = 1; i < 101; i++) {
+        let message : string = "";
+        if (i % 3 == 0 && i % 5 == 0) {
+            message = "FizzBuzz";
+        }
+        else if (i % 3 == 0) {
+            message = "Fizz";
+        }
+        else if (i % 5 == 0) {
+            message = "Buzz";
+        }
+        else {
+            message = i.toString();
+        }
 
-    // Put your code here...
+        console.log(message);
+    }
 
 }
 
